5e858738cc848fed9b8ae8beeb3e3d58f9f34ecb
Gavin Shaw <gavshaw+github@gmail.com>
2024-12-19 17:36:58 +1000

    Setup basic UI layout with basic styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
+import React from 'react';
+
+const Chat = () => {
+    return (
+      <div>
+          <div className="response-div">
+              <div className="response-sub-div">
+                  <span className="response-label">Response</span>
+                  <textarea className="response-textarea"/>
+              </div>
+          </div>
+          <hr/>
+          <div className="system-div">
+              <div className="system-sub-div">
+                  <span className="system-label">System Commands</span>
+                  <textarea className="system-textarea"/>
+              </div>
+          </div>
+          <hr/>
+          <div className="prompt-div">
+              <div className="prompt-sub-div">
+                  <span className="prompt-label">Prompt Input</span>
+                  <textarea className="prompt-textarea"/>
+              </div>
+              <div className="prompt-button">
+                  <button>Submit</button>
+              </div>
+          </div>
+          <hr/>
+      </div>
+    );
+};
+
 function App() {
     return (
         <div>
+            <Chat/>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+import './css/app.css';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App/>);
