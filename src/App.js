cc4964aada172f77594e1e1c1d2ae9c514d55b51
Gavin Shaw <gavshaw+github@gmail.com>
2024-12-22 13:56:22 +1000

    Add ability to submit prompt to up to 3 LLMs (same or different)
    - Each response area allows an LLM to be seected from the dropdown
      allowing for up to 3 different LLMs to be prompted / queried.
    - Because the submitted prompt has a hardcoded 'temperature' of 0.7
      submitting the same prompt to the same LLM multiple times can result
      in variations in the response.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ const Chat = () => {
 
     const [disableButton, setDisableButton] = useState(false);
 
-    const [modelList, setModelList] = useState(['abc', 'def', '123', '456']);
-    const [selectedModel, setSelectedModel] = useState("");
+    const [modelList, setModelList] = useState([]);
+    const [selectedModel1, setSelectedModel1] = useState("");
+    const [selectedModel2, setSelectedModel2] = useState("");
+    const [selectedModel3, setSelectedModel3] = useState("");
 
-    const [response, setResponse] = useState("");
+    const [response1, setResponse1] = useState("");
+    const [response2, setResponse2] = useState("");
+    const [response3, setResponse3] = useState("");
     const [systemCommand, setSystemCommand] = useState("");
     const [prompt, setPrompt] = useState("");
 
@@ -20,21 +24,49 @@ const Chat = () => {
     const models = () => {
         getModels().then(response => {
             setModelList(response);
-            setSelectedModel(response[0]);
+            setSelectedModel1("");
+            setSelectedModel2("");
+            setSelectedModel3("");
         });
     };
 
     const submit = () => {
         setDisableButton(true);
 
-        setResponse("");
+        if (selectedModel1 !== "") {
 
-        submitPrompt(selectedModel, systemCommand, prompt).then(response => {
-            setResponse(response.responseAnswer);
-            if (response.responseSuccess === true) {
-                setPrompt("");
-            }
-        });
+            setResponse1("");
+
+            submitPrompt(selectedModel1, systemCommand, prompt).then(response => {
+                setResponse1(response.responseAnswer);
+                if (response.responseSuccess === true) {
+                }
+            });
+        }
+
+        if (selectedModel2 !== "") {
+
+            setResponse2("");
+
+            submitPrompt(selectedModel2, systemCommand, prompt).then(response => {
+                setResponse2(response.responseAnswer);
+                if (response.responseSuccess === true) {
+                }
+            });
+        }
+
+        if (selectedModel3 !== "") {
+
+            setResponse3("");
+
+            submitPrompt(selectedModel3, systemCommand, prompt).then(response => {
+                setResponse3(response.responseAnswer);
+                if (response.responseSuccess === true) {
+                }
+            });
+        }
+
+
 
         setDisableButton(false);
     };
@@ -44,9 +76,24 @@ const Chat = () => {
           <div className="model-div">
               <div className="model-sub-div">
                   <span className="model-label">Select Model</span>
-                  <select className="model-select">
+                  <select name="llm-1" className="model-select">
+                      <option onClick={(e) => setSelectedModel1("")}></option>
                       {modelList.map(model => (
-                          <option onClick={(e) => setSelectedModel(e.target.value)}>{model}</option>
+                          <option onClick={(e) => setSelectedModel1(e.target.value)}>{model}</option>
+                      ))}
+                  </select>
+                  <span className="model-label">Select Model</span>
+                  <select name="llm-2" className="model-select">
+                      <option onClick={(e) => setSelectedModel2("")}></option>
+                      {modelList.map(model => (
+                          <option onClick={(e) => setSelectedModel2(e.target.value)}>{model}</option>
+                      ))}
+                  </select>
+                  <span className="model-label">Select Model</span>
+                  <select name="llm-3" className="model-select">
+                      <option onClick={(e) => setSelectedModel3("")}></option>
+                      {modelList.map(model => (
+                          <option onClick={(e) => setSelectedModel3(e.target.value)}>{model}</option>
                       ))}
                   </select>
               </div>
@@ -54,7 +101,11 @@ const Chat = () => {
           <div className="response-div">
               <div className="response-sub-div">
                   <span className="response-label">Response</span>
-                  <textarea className="response-textarea" value={response}/>
+                  <textarea className="response-textarea" value={response1}/>
+                  <span className="response-label">Response</span>
+                  <textarea className="response-textarea" value={response2}/>
+                  <span className="response-label">Response</span>
+                  <textarea className="response-textarea" value={response3}/>
               </div>
           </div>
           <hr/>
